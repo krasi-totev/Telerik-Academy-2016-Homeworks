4b4ec000d9981055d292f07d2c3f3ac9190a227c
krasi-totev <krasi.totev@gmail.com>
2016-03-22 17:42:27 +0200

    fixed bin and obj

---
+++ Module 1/C#/Intro-Programming-Homework/HelloCSharp/HelloCSharp.cs
@@ -0,0 +1,12 @@
+﻿//Problem 4. Hello World
+
+//Create, compile and run a “Hello C#” console application.
+//Ensure you have named the application well(e.g. “”HelloCSharp”).
+using System;
+public class HelloCSharp
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello C#");
+    }
+}
---
+++ Module 1/C#/Intro-Programming-Homework/PrintLongSequence/PrintLongSequence.cs
@@ -0,0 +1,30 @@
+﻿//Problem 16.* Print Long Sequence
+
+//Write a program that prints the first 1000 members of the sequence: 2, -3, 4, -5, 6, -7, …
+//You might need to learn how to use loops in C# (search in Internet).
+using System;
+public class PrintLongSequence
+{
+    static void Main()
+    {
+        int count = 0;
+
+        for (int i = 2; i <= 1001; i++)
+        {
+            count++;
+            if (count % 10 == 0)
+            {
+                Console.WriteLine();
+            }
+            if (i % 2 != 0)
+            {
+                Console.Write(i - 2 * i + ", ");
+            }
+            else
+            {
+                Console.Write(i + ", ");
+            }
+        }
+        Console.WriteLine();
+    }
+}

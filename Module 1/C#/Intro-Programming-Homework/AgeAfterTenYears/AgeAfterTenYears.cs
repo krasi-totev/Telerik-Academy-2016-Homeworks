f2e9581fdf9bf9a046b5ea8685de0d57f04b9911
krasi-totev <krasi.totev@gmail.com>
2016-03-22 17:38:25 +0200

    C# Part 1 - Homwerk 1

---
+++ Module 1/C#/Intro-Programming-Homework/AgeAfterTenYears/AgeAfterTenYears.cs
@@ -0,0 +1,35 @@
+﻿//Problem 15.* Age after 10 Years
+
+//Write a program to read your birthday from the console and print how old you are now and how old you will be after 10 years.
+using System;
+class AgeAfterTenYears
+{
+    static void Main()
+    {
+        Console.WriteLine("What is your birth date?");
+        Console.Write("Day: ");
+        string day = Console.ReadLine();
+
+        Console.Write("Month (with digits): ");
+        string month = Console.ReadLine();
+
+        Console.Write("Year: ");
+        string year = Console.ReadLine();
+
+        string myBirthday = month + "/" + day + "/" + year;
+        DateTime birthDate = DateTime.Parse(myBirthday);
+        DateTime today = DateTime.Today;
+
+        int age = today.Year - birthDate.Year;
+
+        if (today.Date < birthDate.Date)
+        {
+            age -= 1;
+        }
+        Console.WriteLine("-----------------------");
+        Console.WriteLine(" Born: " + birthDate.ToString("dd MMMM yyyy"));
+        Console.WriteLine("-----------------------");
+        Console.WriteLine(" Age: " + age + " years old!");
+        Console.WriteLine("-----------------------");
+    }
+}
---
+++ Module 1/C#/Intro-Programming-Homework/CurrentDateAndTime/CurrentDateAndTime.cs
@@ -0,0 +1,12 @@
+﻿//Problem 14.* Current Date and Time
+
+//Create a console application that prints the current date and time.Find out how in Internet.
+using System;
+public class CurrentDateAndTime
+{
+    static void Main()
+    {
+        DateTime today = DateTime.Now;
+        Console.WriteLine("Today is: " + today.ToString("dd MMM yyyy"));
+    }
+}
